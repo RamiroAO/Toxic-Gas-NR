9b21a438cd0f121961774eec8a4c4a5e919bdfc0
Ramiro Aguilar Ordaz <ramiro1710@hotmail.com>
2021-05-18 23:54:00 -0500

    newAcc

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_BYoSMwYtI', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '47r911uvpdde9jt1aserpvrs8v' , // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_9TBzhgf3T', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2cc9c9fesaqt64tft0dlajtbl8' , // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1'// e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://cquhu03qnc.execute-api.us-east-1.amazonaws.com/prod'// e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://pzmr0sou07.execute-api.us-east-1.amazonaws.com/prod'// e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
